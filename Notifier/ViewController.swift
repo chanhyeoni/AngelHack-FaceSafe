54e9faae6278fbbf12ee8ed5bb0bafbe921f06e9
kang ki-hoon <tachyon@tachyons-macbook-pro.local>
2017-06-11 01:06:25 -0400

    Initial Commit

---
+++ Notifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notifier
+//
+//  Created by kang ki-hoon on 2017. 6. 11..
+//  Copyright © 2017년 kang ki-hoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
